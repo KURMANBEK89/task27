cf5163004bac6050530d51bf1d32d26f1f69b8db
kurmanbek89 <kumakary2@gmail.com>
2022-11-23 18:49:40 +0300

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sqrt(9));
+    }
+
+    public static UnaryOperator<Double> sqrt(int a) {
+        UnaryOperator<Double> unaryOperator = new UnaryOperator<Double>() {
+            @Override
+            public Double apply(Double aDouble) {
+                return Math.sqrt(aDouble);
+            }
+        };
+        unaryOperator.apply((double)a);
+        return unaryOperator;
+
+
     }
 }
